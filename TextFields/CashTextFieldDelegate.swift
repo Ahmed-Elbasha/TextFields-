f775647471e3d5fc9de7a577e631bf4d2446e621
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-07-05 18:10:56 +0200

    Added CashTextFieldDelegate class

--- TextFields/CashTextFieldDelegate.swift
+++ TextFields/CashTextFieldDelegate.swift
@@ -10,5 +10,33 @@ import Foundation
 import UIKit
 
 class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = "0"
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        if textField.text == "0" {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        let floatingNumber : NSNumber = (Float(newText as String)! as? NSNumber)!
+        let numberFormatter = NumberFormatter()
+        numberFormatter.positiveFormat = "###0.#"
+        let stringFormattedFloatNumber = numberFormatter.string(from: floatingNumber)
+        textField.text = stringFormattedFloatNumber
+        return true
+    }
+    
 }
